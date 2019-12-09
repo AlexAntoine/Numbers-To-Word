27da5024f7cec5898fb6ae70d9c7e82d8510d233
macbook <macbook@10.0.0.164>
2019-12-08 21:48:40 -0500

    First commit

--- FinalCapstone/src/Capstone/NumbersToWords.java
+++ FinalCapstone/src/Capstone/NumbersToWords.java
@@ -1,37 +1,24 @@
-/************ IT 2789 **********************************************************************************
-*****************************************************************************************************
-** **
-** This program was prepared as part of the requirements for IT 2789, Structured Programming. **
-** **
-** COURSE DESCRIPTION: **
-** This is the second of a three course programming sequence. IT 2789, Structured Programming, builds on the **
-** concepts learned in IT 2100, Programming Logic. Programming Logic introduces programming concepts without **
-** any coding syntax. **
-** **
-** Structured Programming expands the student’s knowledge of programming concepts in the context of a **
-** programming language and its integrated development environment. It expands the student’s knowledge of **
-** algorithm design while introducing structured/modular programming in the context of a programming language. **
-** It explores implementing the basic programming concepts of variables (primitive and abstract), data types, strings, **
-** arithmetic and logical operators, decisions, and loops. **
-** **
-** In this course the student begin implementing algorithms. Students learn to develop applications in ways that **
-** facilitate program maintenance by producing self-documenting code as well as using program comments properly. **
-** **
-** In addition, this course continues to develop basic problem solving analysis and critical thinking skills that aid the **
-** student when developing algorithms. **
-** **
-** This course does not include Object Oriented Programming, which is included in the next course in the sequence. **
-** **
-** This course does not include the use of Graphical User Interfaces, which is included in the next course in the **
-** sequence. **
-** **/
-
 package Capstone;
 
 public class NumbersToWords
 {
 
 	public NumbersToWords()
+	{
+		
+	}
+	
+	public char findChar(String number)
+	{
+		
+	}
+	
+	public boolean decimals(String number)
+	{
+		
+	}
+	
+	public String rounding(double number, int place)
 	{
 		
 	}
